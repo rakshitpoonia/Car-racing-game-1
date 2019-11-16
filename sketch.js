6061768dd848597aaac2a67fe74b90e21a1a2c0e
rakshitpoonia <rakshitpoonia2004@gmail.com>
2019-11-16 18:10:08 +0530

    multiplayer

--- sketch.js
+++ sketch.js
@@ -1,29 +1,43 @@
-var ball;
+var ball,database;
 
 function setup(){
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    database=firebase.database();
+    var ballPosition=database.ref('Ball/position');
+    ballPosition.on("value",readposition,error);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+       writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    
+    database.ref('Ball/position').set({
+        'x':position.x+x,
+        'y':position.y+y,
+    })
+}
+function readposition(data){
+position=data.val();
+ball.x=position.x;
+ball.y=position.y;
+}
+function error(){
+    console.log("error in writing to database");
 }
